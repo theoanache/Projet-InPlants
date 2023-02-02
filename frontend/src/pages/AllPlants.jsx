416502cf99cd555176ddc340727288e5ea5b4093
theo anache <theoo.anache@gmail.com>
2023-02-02 10:20:29 +0100

    add input page update plante + appel axios .put

--- frontend/src/pages/AllPlants.jsx
+++ frontend/src/pages/AllPlants.jsx
@@ -1,9 +1,37 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { useParams } from "react-router-dom";
 import ImgCard from "../components/ImgCard";
 
 function AllPlants() {
+  const { id } = useParams();
   const [plants, setPlants] = useState([]);
+  const [plante, setPlante] = useState({
+    name: "",
+    description: "",
+    Origin: "",
+    sunshine: "",
+    arrosage: "",
+    feuillage: "",
+  });
+
+  const handleChange = (event) => {
+    setPlante({ ...plante, [event.target.name]: event.target.value });
+  };
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    axios
+      .put(`http://localhost:5000/plants/${id}`, {
+        name: plante.name,
+        description: plante.description,
+        Origin: plante.Origin,
+        sunshine: plante.sunshine,
+        arrosage: plante.arrosage,
+        feuillage: plante.feuillage,
+      })
+      .then((res) => res);
+  };
 
   useEffect(() => {
     axios
@@ -18,18 +46,94 @@ function AllPlants() {
         {plants.map((plant) => (
           <div
             key={plant.id}
-            className="flex md:drop-shadow-xl rounded-lg bg-white  w-[48%] h-[220px]"
+            className="flex md:drop-shadow-xl rounded-lg bg-white w-[80%] h-[430px]"
           >
-            <div className="max-w-[150px] pl-4 h-full flex justify-center items-center  ">
+            <div className="max-w-[200px] min-w-[200px] pl-4 h-full flex justify-center items-center  ">
               <ImgCard image={plant.image} />
             </div>
-            <div className="text-sm">
-              <h4>{plant.name}</h4>
-              <p>{plant.description}</p>
-              <h4>{plant.Origin}</h4>
-              <h4>{plant.sunshine}</h4>
-              <h4>{plant.arrosage}</h4>
-              <h4>{plant.feuillage}</h4>
+            <div className="text-sm text-black flex flex-col w-[75%]">
+              <div className="flex flex-col mb-1">
+                <label className="font-semibold text-grey" htmlFor="Nom">
+                  Nom
+                </label>
+                <input
+                  className="pl-1 border-2 border-gray  rounded-lg"
+                  type="text"
+                  name="nom"
+                  defaultValue={plant.name}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="flex flex-col mb-1 ">
+                <label
+                  className="font-semibold text-grey"
+                  htmlFor="description"
+                >
+                  description
+                </label>
+                <textarea
+                  className="pl-1 border-2 border-gray  rounded-lg h-28"
+                  type="text"
+                  name="description"
+                  defaultValue={plant.description}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="flex flex-col mb-1">
+                <label className="font-semibold text-grey" htmlFor="Origine">
+                  Origine
+                </label>
+                <input
+                  className="pl-1 border-2 border-gray rounded-lg"
+                  type="text"
+                  name="Origine"
+                  defaultValue={plant.Origin}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="flex flex-col mb-1">
+                <label className="font-semibold text-grey" htmlFor="sunshine">
+                  Ensoleillement
+                </label>
+                <input
+                  className="pl-1 border-2 border-gray rounded-lg"
+                  type="text"
+                  name="sunshine"
+                  defaultValue={plant.sunshine}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="flex flex-col mb-1">
+                <label className="font-semibold text-grey" htmlFor="arrosage">
+                  arrosage
+                </label>
+                <input
+                  className="pl-1 border-2 border-gray  rounded-lg"
+                  type="text"
+                  name="arrosage"
+                  defaultValue={plant.arrosage}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="flex flex-col mb-1">
+                <label className="font-semibold text-grey" htmlFor="feuillage">
+                  feuillage
+                </label>
+                <input
+                  className="pl-1 border-2 border-gray  rounded-lg"
+                  type="text"
+                  name="feuillage"
+                  defaultValue={plant.feuillage}
+                  onChange={handleChange}
+                />
+              </div>
+              <button
+                type="button"
+                className="bg-green text-white font-bold text-lg py-2 w-[20%] rounded"
+                onClick={handleClick}
+              >
+                Modifier
+              </button>
             </div>
           </div>
         ))}
